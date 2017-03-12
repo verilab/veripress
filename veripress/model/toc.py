e69f5460276e51b026f2c7917d728a6425cbc24d
Richard Chien <richardchienthebest@gmail.com>
2017-03-12 17:37:40 +0800

    Finish 'toc' module

---
+++ veripress/model/toc.py
@@ -0,0 +1,185 @@
+import re
+from html.parser import HTMLParser
+
+
+class _HtmlHeaderNode(object):
+    """Represents a header element when parsing the HTML string."""
+
+    def __init__(self, level):
+        """Initialize attributes."""
+        self.level = level  # header level of the element, e.g. 1 for <h1>, 2 for <h2>, etc
+        self.id = ''  # anchor id (in-page link), used in 'id' and 'href' attribute of 'a' tag
+        self.data = ''  # content of header tag, e.g. 'Title' for '<h1>Title</h1>'
+        self.father = None  # point to the direct father node
+        self.children = []  # elements with lower levels that directly follows the current elem
+
+    def to_dict(self):
+        """Convert self to a dict object for serialization."""
+        return {
+            'level': self.level,
+            'id': self.id,
+            'data': self.data,
+            'children': [child.to_dict() for child in self.children]
+        }
+
+
+class HtmlTocParser(HTMLParser):
+    """Parse table of content from a given HTML string."""
+
+    # regular expression for replacing all punctuations in the header with '-'
+    _punctuations_exp = re.compile(
+        '[\s\u0020-\u002f\u003a-\u0040\u005b-\u0060\u007b-\u007e\u00a0-\u00bf'
+        '\u2000-\u206f\u2e00-\u2e7f\u3000-\u303f\uff01-\uff0f\uff1a-\uff20'
+        '\uff3b-\uff40\uff5b-\uff65\uffe0-\uffe6\uffe8-\uffec\ufe10-\ufe1f]+'
+    )
+
+    def __init__(self):
+        """Initialize attributes."""
+        super().__init__(convert_charrefs=False)
+        self._root = _HtmlHeaderNode(level=0)  # root node with no data of itself, only 'children' matters
+        self._curr_node = self._root  # most recently handled header node
+        self._in_header = False
+        self._header_id_count = {}  # record header ids to avoid collisions
+        self._html = ''  # full HTML string parsed
+        self._temp_html = ''  # temporary HTML string of this current header node
+
+    def toc(self, depth=6, lowest_level=6):
+        """
+        Get table of content of currently fed HTML string.
+
+        :param depth: the depth of TOC
+        :param lowest_level: the allowed lowest level of header tag
+        :return: a list representing the TOC
+        """
+        depth = min(max(depth, 0), 6)  # make depth >= 0 and <= 6
+        depth = 6 if depth == 0 else depth  # depth == 0 <=> depth == 6
+        lowest_level = min(max(lowest_level, 1), 6)  # make lowest_level >= 1 and <= 6
+        toc = self._root.to_dict()['children']
+
+        def traverse(curr_toc: list, dep, lowest_lvl, curr_depth=1):
+            if curr_depth > dep:
+                # clear all items of this depth and exit the recursion
+                curr_toc.clear()
+                return
+
+            items_to_remove = []
+            for item in curr_toc:
+                if item['level'] > lowest_lvl:
+                    # record item with low header level, for removing it later
+                    items_to_remove.append(item)
+                else:
+                    traverse(item['children'], dep, lowest_lvl, curr_depth + 1)
+            [curr_toc.remove(item) for item in items_to_remove]
+
+        traverse(toc, depth, lowest_level)
+        return toc
+
+    def toc_html(self, depth=6, lowest_level=6):
+        """
+        Get TOC of currently fed HTML string in form of HTML string.
+
+        :param depth: the depth of TOC
+        :param lowest_level: the allowed lowest level of header tag
+        :return: an HTML string
+        """
+        toc = self.toc(depth=depth, lowest_level=lowest_level)
+        if not toc:
+            return ''
+
+        def map_toc_list(toc_list):
+            result = ''
+            if toc_list:
+                result += '<ul>\n'
+                result += ''.join(map(lambda x: '<li>'
+                                                '<a href="#{}">{}</a>{}'
+                                                '</li>\n'.format(x['id'], x['data'], map_toc_list(x['children'])),
+                                      toc_list))
+                result += '</ul>'
+            return result
+
+        return map_toc_list(toc)
+
+    @property
+    def html(self):
+        """
+        The parsed HTML string with additional a tag marking the header anchors.
+
+        Example:
+            `<h1>Title</h1>`
+
+            becomes:
+
+            `<h1><a id="Title" class="anchor"></a>Title</h1>`
+        """
+        return self._html
+
+    @staticmethod
+    def _get_level(tag):
+        """Match the header level in the given tag name, or None if it's not a header tag."""
+        m = re.fullmatch('h([123456])', tag, flags=re.IGNORECASE)
+        if not m:
+            return None
+        return int(m.group(1))
+
+    def handle_starttag(self, tag, attrs):
+        curr_tag = '<{}{}{}>'.format(tag, ' ' if attrs else '',
+                                     ' '.join(['{}="{}"'.format(*attr) for attr in attrs]))
+
+        level = self._get_level(tag)
+        if level is not None:
+            self._in_header = True
+            new_node = _HtmlHeaderNode(level=level)
+            while new_node.level <= self._curr_node.level:
+                # the new node is a higher level header, e.g. new: h2, curr: h4
+                # so we return back until the current is higher
+                self._curr_node = self._curr_node.father
+            new_node.father = self._curr_node  # assign the new node as a child of the current node
+            self._curr_node.children.append(new_node)
+            self._curr_node = new_node
+            self._temp_html = curr_tag
+            return
+
+        self._html += curr_tag
+
+    def handle_startendtag(self, tag, attrs):
+        self._html += '<{}{}{} />'.format(tag, ' ' if attrs else '',
+                                          ' '.join(['{}="{}"'.format(*attr) for attr in attrs]))
+
+    def handle_endtag(self, tag):
+        curr_tag = '</{}>'.format(tag)
+
+        if self._get_level(tag) is not None:
+            header_id = self._punctuations_exp.sub('-', self._curr_node.data).strip('-')
+            count = self._header_id_count.setdefault(header_id, 0)
+            self._header_id_count[header_id] += 1
+            if count > 0:
+                header_id += '_%d' % count
+
+            self._curr_node.id = header_id
+            self._html += self._temp_html  # start tag of the current header node
+            self._html += '<a id="{0}" href="#{0}" class="anchor"></a>'.format(header_id)  # anchor
+            self._html += self._curr_node.data + curr_tag  # header content and end tag
+            self._temp_html = ''
+            self._in_header = False
+            return
+
+        self._html += curr_tag
+
+    def handle_data(self, data):
+        if self._in_header:
+            self._curr_node.data += data
+            return
+
+        self._html += data
+
+    def handle_comment(self, data):
+        self._html += '<!--{}-->'.format(data)
+
+    def handle_entityref(self, name):
+        self._html += '&{};'.format(name)
+
+    def handle_charref(self, name):
+        self._html += '&#{};'.format(name)
+
+    def error(self, message):
+        pass
---
+++ veripress_tests/test_toc.py
@@ -0,0 +1,66 @@
+import re
+
+from veripress.model.toc import HtmlTocParser
+
+
+def html_same(html1, html2):
+    return re.sub('\s', '', html1) == re.sub('\s', '', html2)
+
+
+html_example = """
+<h6>a very small title</h6>
+<h1>Title</h1>
+  <h2>Title</h2>
+    <h4>Another title, yes!</h4>
+    <h3>中文，标题 Title&amp;</h3>
+      <p>a random paragraph...<br/></p>
+      &amp; &#60;
+      <!-- comment -->
+<h1>Another-h1-1</h1>
+  <h5>a very small title</h5>
+"""
+
+
+def test_toc_parser():
+    parser = HtmlTocParser()
+    parser.feed('<a href="#">no-effect</a>')
+    assert html_same(parser.html, '<a href="#">no-effect</a>')
+
+    parser.feed('<h1>Title</h1>')
+    assert html_same(parser.html,
+                     '<a href="#">no-effect</a><h1><a id="Title" href="#Title" class="anchor"></a>Title</h1>')
+
+    parser = HtmlTocParser()
+    parser.feed(html_example)
+    expected_toc = [{'level': 6, 'id': 'a-very-small-title', 'data': 'a very small title', 'children': []},
+                    {'level': 1, 'id': 'Title', 'data': 'Title', 'children': [
+                        {'level': 2, 'id': 'Title_1', 'data': 'Title', 'children': [
+                            {'level': 4, 'id': 'Another-title-yes', 'data': 'Another title, yes!', 'children': []},
+                            {'level': 3, 'id': '中文-标题-Title', 'data': '中文，标题 Title', 'children': []}
+                        ]}
+                    ]},
+                    {'level': 1, 'id': 'Another-h1-1', 'data': 'Another-h1-1', 'children': [
+                        {'level': 5, 'id': 'a-very-small-title_1', 'data': 'a very small title', 'children': []}
+                    ]}]
+    assert parser.toc() == expected_toc
+
+    expected_toc_html = """
+    <ul>
+      <li><a href="#Title">Title</a>
+        <ul>
+          <li><a href="#Title_1">Title</a></li>
+        </ul>
+      </li>
+      <li><a href="#Another-h1-1">Another-h1-1</a>
+        <ul>
+          <li><a href="#a-very-small-title_1">a very small title</a></li>
+        </ul>
+      </li>
+    </ul>
+    """
+    assert html_same(parser.toc_html(depth=2, lowest_level=5), expected_toc_html)
+
+    parser = HtmlTocParser()
+    parser.feed('')
+    assert parser.toc() == []
+    assert parser.toc_html() == ''
