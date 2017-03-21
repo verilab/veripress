8079b9253e50fd2b6a8e51e84fab96e6da815685
Richard Chien <richardchienthebest@gmail.com>
2017-03-21 21:01:20 +0800

    Add docker image

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.6.0
+MAINTAINER Richard Chien <richardchienthebest@gmail.com>
+
+RUN mkdir /veripress
+WORKDIR /veripress
+COPY veripress veripress
+COPY veripress_cli veripress_cli
+COPY setup.py setup.py
+COPY MANIFEST.in MANIFEST.in
+RUN pip install . gevent
+
+VOLUME ["/instance"]
+WORKDIR /instance
+ENTRYPOINT ["veripress"]
--- after-travis-ci-success.sh
+++ after-travis-ci-success.sh
@@ -3,3 +3,8 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
     ./node_modules/.bin/git-update-ghpages -e veripress/demo veripress_demo_instance/_deploy;
     ./node_modules/.bin/git-update-ghpages -e veripress/docs veripress_docs/_deploy;
 fi
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    if [[ $TRAVIS_BRANCH =~ ^v[0-9.]+$ ]]; then docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:latest; fi
+    docker push $DOCKER_REPO
+fi
