de65af2c69712f3561324516a57a80b8f280f9a4
Richard Chien <richardchienthebest@gmail.com>
2017-03-24 22:08:13 +0800

    Change demo instance folder name and fix bugs of some templates

--- after-travis-ci-success.sh
+++ after-travis-ci-success.sh
@@ -1,6 +1,6 @@
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
     npm install git-update-ghpages;
-    ./node_modules/.bin/git-update-ghpages -e veripress/demo veripress_demo_instance/_deploy;
+    ./node_modules/.bin/git-update-ghpages -e veripress/demo veripress_demo/_deploy;
     ./node_modules/.bin/git-update-ghpages -e veripress/docs veripress_docs/_deploy;
 fi
 
