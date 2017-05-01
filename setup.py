80d5d7181cc1f1363bcc4970192375bb07c81246
Richard Chien <richardchienthebest@gmail.com>
2017-05-01 14:25:06 +0800

    Increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='veripress',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     url='https://github.com/veripress/veripress',
     license='MIT License',
