02adb98e406595157a4cfeaa8481d81e51f756c4
Richard Chien <richardchienthebest@gmail.com>
2017-05-01 14:12:28 +0800

    Fix markdown parsing when no lang is specified in codeblock

--- veripress/model/parsers.py
+++ veripress/model/parsers.py
@@ -131,7 +131,7 @@ class MarkdownParser(Parser):
 
         def block_code(self, code, lang=None):
             if not lang:
-                return '\n<pre><code>{}</code></pre>\n'.format(mistune.escape(code))
+                return '\n<pre>{}</pre>\n'.format(mistune.escape(code))
             lexer = get_lexer_by_name(lang, stripall=True)
             formatter = html.HtmlFormatter()
             return highlight(code, lexer, formatter)
