a16774e050b6d18aaa1907baa37163e8d7e820f8
Richard Chien <richardchienthebest@gmail.com>
2017-05-24 13:46:13 +0800

    Add version option

--- veripress_cli/__init__.py
+++ veripress_cli/__init__.py
@@ -1,7 +1,13 @@
 import click
 
-cli = click.Group(help='This is a blog engine for hackers. '
-                       'You can use this to serve a blog, a wiki or anything else you like.')
+
+@click.group(name='veripress',
+             short_help='A blog engine for hackers.',
+             help='This is a blog engine for hackers. '
+                  'You can use this to serve a blog, a wiki or anything else you like.')
+@click.version_option(version='1.0.5')
+def cli():
+    pass
 
 
 def main():
