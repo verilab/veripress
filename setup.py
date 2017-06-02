73215327124108bd11384590f4feb908973b9d27
Richard Chien <richardchienthebest@gmail.com>
2017-06-02 21:04:38 +0800

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='veripress',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     url='https://github.com/veripress/veripress',
     license='MIT License',
--- veripress_cli/__init__.py
+++ veripress_cli/__init__.py
@@ -5,7 +5,7 @@ import click
              short_help='A blog engine for hackers.',
              help='This is a blog engine for hackers. '
                   'You can use this to serve a blog, a wiki or anything else you like.')
-@click.version_option(version='1.0.5')
+@click.version_option(version='1.0.6')
 def cli():
     pass
 
