3b642fce50057762523abcc54509a052331f218a
Richard Chien <richardchienthebest@gmail.com>
2018-06-10 18:41:48 +0800

    Fix bug

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='veripress',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     url='https://github.com/veripress/veripress',
     license='MIT License',
     author='Richard Chien',
     author_email='richardchienthebest@gmail.com',
     description='A blog engine for hackers.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'Flask', 'Flask-Caching', 'PyYAML', 'markdown',
         'Pygments', 'pytz'
     ],
+    python_requires='>=3.4',
     include_package_data=True,
     platforms='any',
     entry_points=dict(
--- veripress_cli/deploy.py
+++ veripress_cli/deploy.py
@@ -9,7 +9,7 @@ from veripress_cli.helpers import makedirs
 
 
 def validate_repo_name(ctx, param, value):
-    m = re.fullmatch('([_\-A-Z0-9a-z]+)/([_\-A-Z0-9a-z]+)', value)
+    m = re.fullmatch('([._\-A-Z0-9a-z]+)/([._\-A-Z0-9a-z]+)', value)
     if not m:
         raise click.BadParameter('The repo name is invalid.')
     return m.groups()
@@ -17,11 +17,14 @@ def validate_repo_name(ctx, param, value):
 
 @cli.command('setup-github-pages', short_help='Setup GitHub Pages.',
              help='This command will setup the deploy folder as a GitHub Pages repo.')
-@click.option('--repo', '-r', prompt='Please enter your GitHub repo name, e.g. "someone/the-repo"',
-              help='The GitHub repo name you want to deploy to.', callback=validate_repo_name)
+@click.option('--repo', '-r',
+              prompt='Please enter your GitHub repo name, e.g. "someone/the-repo"',
+              help='The GitHub repo name you want to deploy to.',
+              callback=validate_repo_name)
 @click.option('--name', '-n', prompt='Please enter your name (for git config)',
               help='Your name set in git config.')
-@click.option('--email', '-e', prompt='Please enter your email (for git config)',
+@click.option('--email', '-e',
+              prompt='Please enter your email (for git config)',
               help='Your email set in git config')
 def setup_command(repo, name, email):
     user, repo = repo
@@ -30,10 +33,11 @@ def setup_command(repo, name, email):
     deploy_dir = get_deploy_dir()
     makedirs(deploy_dir, mode=0o755, exist_ok=True)
 
-    os.system('git -C {} init'.format(deploy_dir))
-    os.system('git -C {} config user.email "{}"'.format(deploy_dir, email))
-    os.system('git -C {} config user.name "{}"'.format(deploy_dir, name))
-    os.system('git -C {} remote add origin git@github.com:{}/{}.git'.format(deploy_dir, user, repo))
+    os.system('git -C "{}" init'.format(deploy_dir))
+    os.system('git -C "{}" config user.email "{}"'.format(deploy_dir, email))
+    os.system('git -C "{}" config user.name "{}"'.format(deploy_dir, name))
+    os.system('git -C "{}" remote add origin git@github.com:{}/{}.git'.format(
+        deploy_dir, user, repo))
 
 
 @cli.command('deploy', short_help='Deploy GitHub Pages.',
@@ -43,13 +47,17 @@ def deploy_command():
     deploy_dir = get_deploy_dir()
     makedirs(deploy_dir, mode=0o755, exist_ok=True)
 
-    os.system('git -C {} add .'.format(deploy_dir))
-    if os.system('git -C {} diff --quiet --exit-code'.format(deploy_dir)) == 0 \
-            and os.system('git -C {} diff --quiet --cached --exit-code'.format(deploy_dir)) == 0:
+    os.system('git -C "{}" add .'.format(deploy_dir))
+    if os.system(
+            'git -C "{}" diff --quiet --exit-code'.format(deploy_dir)) == 0 \
+            and os.system(
+        'git -C "{}" diff --quiet --cached --exit-code'.format(
+            deploy_dir)) == 0:
         click.echo('There are no changes to be deployed.')
         return
 
     dt = datetime.now()
-    os.system('git -C {} commit -m "Updated on {} at {}"'.format(deploy_dir,
-                                                                 dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S')))
-    os.system('git -C {} push origin master'.format(deploy_dir))
+    os.system('git -C "{}" commit -m "Updated on {} at {}"'.format(
+        deploy_dir, dt.strftime('%Y-%m-%d'), dt.strftime('%H:%M:%S')))
+    os.system(
+        'git -C "{}" push --set-upstream origin master'.format(deploy_dir))
