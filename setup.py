9710bb42a0e4c98abeaabb1fe1a9785212ed0297
Richard Chien <richardchienthebest@gmail.com>
2018-08-25 18:05:41 +0800

    Update project files

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ setup(
     author_email='richardchienthebest@gmail.com',
     description='A blog engine for hackers.',
     install_requires=[
-        'Flask', 'Flask-Caching', 'PyYAML', 'markdown',
-        'Pygments', 'pytz'
+        'Flask>=1.0.2', 'Werkzeug>=0.14.1', 'Flask-Caching>=1.4.0',
+        'PyYAML>=3.13', 'markdown>=2.6.11', 'Pygments>=2.2.0',
+        'pytz>=2018.5', 'click>=6.7'
     ],
     python_requires='>=3.4',
     include_package_data=True,
