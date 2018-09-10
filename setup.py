0cd5322e181fb856092a106cb597a9bc68519723
Richard Chien <richardchienthebest@gmail.com>
2018-09-10 12:17:48 +0800

    Bump version to 1.0.9

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='veripress',
-    version='1.0.8',
+    version='1.0.9',
     packages=find_packages(),
     url='https://github.com/veripress/veripress',
     license='MIT License',
     author='Richard Chien',
     author_email='richardchienthebest@gmail.com',
     description='A blog engine for hackers.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'Flask', 'Flask-Caching', 'PyYAML', 'markdown',
         'Pygments', 'pytz'
@@ -18,5 +23,17 @@ setup(
     platforms='any',
     entry_points=dict(
         console_scripts=['veripress=veripress_cli:main']
+    ),
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Flask',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     )
 )
